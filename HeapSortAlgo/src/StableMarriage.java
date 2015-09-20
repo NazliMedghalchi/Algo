1858d2dec047daa5350e5bf0bb8017dea03a11ec
Nazli Medghalchi <medghalchi.n@gmail.com>
2015-09-20 16:57:43 -0400

    Create Sets of Applicants and Companies for stable algo

--- HeapSortAlgo/src/StableMarriage.java
+++ HeapSortAlgo/src/StableMarriage.java
@@ -1,5 +1,14 @@
+import java.util.ArrayList;
+
 /**
  * Created by Nazli on 2015-09-20.
  */
 public class StableMarriage {
+    ArrayList<Integer> applicants = new ArrayList<Integer>();
+    ArrayList<Integer> company = new ArrayList<Integer>();
+    ArrayList<Integer> ordering = new ArrayList<Integer>();
+    public ArrayList<Integer> StableMarriage(){
+
+        return ordering;
+    }
 }
